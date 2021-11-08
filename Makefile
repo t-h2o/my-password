05230292df85d11dcc540074ebcbcb882c518f4b
Theo Grivel <tgrivel@c1r16s7.42lausanne.ch>
2021-11-08 10:53:54 +0100

    No need librairy

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 DIR_SRC	=	./src/
+DIR_BIN	=	./bin/
 
-SRCS	=	${DIR_SRC}main.c
+SRCS	=	${DIR_SRC}main.c ${DIR_SRC}ft_atoi.c \
+			${DIR_SRC}ft_isdigit.c ${DIR_SRC}ft_strdup.c \
+			${DIR_SRC}ft_strlcpy.c ${DIR_SRC}ft_strlen.c \
+			${DIR_SRC}ft_substr.c
 
 OBJS	=	${SRCS:.c=.o}
 
@@ -19,7 +23,7 @@ LIB	=	-lft
 	${CC} ${FLAGS} -c $< -o ${<:.c=.o}
 
 ${NAME}:	${OBJS}
-	${CC} ${FLAGS} -o ${NAME} ${OBJS} -L. -lft
+	${CC} ${FLAGS} -o ${DIR_BIN}${NAME} ${SRCS} 
 
 all:		${NAME}
 
@@ -27,6 +31,6 @@ clean:
 	${RM} ${OBJS}
 
 fclean:		clean
-	${RM} ${NAME}
+	${RM} ${DIR_BIN}${NAME}
 
 re:		fclean all
--- src/libft.h
+++ src/libft.h
@@ -6,68 +6,30 @@
 /*   By: tgrivel <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:05:32 by tgrivel           #+#    #+#             */
-/*   Updated: 2021/11/05 20:00:07 by tgrivel          ###   ########.fr       */
+/*   Updated: 2021/11/08 10:52:14 by tgrivel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # include	<stddef.h>
 # include	<stdlib.h>
-# include	<unistd.h>
+# include	<stdio.h>
 # define LIBFT_H
-# define FT_32B_MAX		2147483647
-# define FT_32B_MIN		-2147483648
-# define FT_U_32B_MAX	4294967295
-# define FT_64B_MAX		9223372036854775807
-# define FT_64B_MIN		-9223372036854775808
-# define FT_U_64B_MAX	18446744073709551615
-
-	// Type define
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
 
 	// whithout extern functions
 
-int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *b, size_t len);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
-size_t	ft_strlcat(char *dest, const char *src, size_t n);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 int		ft_atoi(const char *str);
 
 	// only malloc function
 
-void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
 	// Other
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 	// write in the file descriptor
 
@@ -76,9 +38,4 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(int n, int fd);
 
-	// BONUS
-
-t_list	*ft_lst_new(void *content);
-int		ft_lstsize(t_list *lst);
-
-#endif
+#endif /* LIBFT_H */
